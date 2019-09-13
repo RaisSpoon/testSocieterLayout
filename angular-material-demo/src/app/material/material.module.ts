8c861e8379715aa7a3229c26fc4081d109a94020
Raissah2407 <raissah.oderuth@sc-mauritius.com>
2019-09-13 17:32:21 +0400

    layout

--- angular-material-demo/src/app/material/material.module.ts
+++ angular-material-demo/src/app/material/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import{MatButtonModule} from '@angular/material';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
 
-const MaterialComponents = [MatButtonModule,MatMenuModule,MatFormFieldModule
+const MaterialComponents = [MatButtonModule,MatMenuModule,MatFormFieldModule,MatCardModule
 ]
 
 
