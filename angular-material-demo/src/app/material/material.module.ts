a2d56e50be0b969d3177b2cb076caff869318b83
Raissah2407 <raissah.oderuth@sc-mauritius.com>
2019-09-16 16:12:08 +0400

    update files

--- angular-material-demo/src/app/material/material.module.ts
+++ angular-material-demo/src/app/material/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import{MatButtonModule} from '@angular/material';
+import{MatButtonModule, MatTabsModule} from '@angular/material';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatCardModule} from '@angular/material/card';
 
-const MaterialComponents = [MatButtonModule,MatMenuModule,MatFormFieldModule,MatCardModule
+const MaterialComponents = [MatButtonModule,MatMenuModule,MatFormFieldModule,MatCardModule, MatTabsModule
 ]
 
 
